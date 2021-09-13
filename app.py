3951fda449583ec00372e31c32253228697b0e4a
Kathir Sudan <52427761+Ex-comunicado@users.noreply.github.com>
2021-09-13 17:00:58 +0530

    Created login and register forms

--- app.py
+++ app.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 
 app = Flask(__name__)
+app.config['WTF_CSRF_ENABLED'] = False
+
+#------------------Forms------------------
+class RegisterForm(FlaskForm):    
+    username = StringField("Username: ")
+    email_addr = StringField("E-mail Address: ")
+    password1 = PasswordField("Password: ") 
+    password2 = PasswordField("Confirm Password: ")
+    submit = SubmitField("Create Account")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username: ")
+    password = PasswordField("Password: ")
+    submit = SubmitField("Login")
+#------------------Forms------------------
 
 #------------------Routes------------------
 @app.route('/')
@@ -11,15 +29,13 @@ def home_page():
 def about_us():
     return render_template('aboutus.html')
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
-    return render_template('register.html')
+    form = RegisterForm()
+    return render_template('register.html', form=form)
 #------------------Routes------------------
-
-#------------------Forms------------------
-# Add form code here
-#------------------Forms------------------
