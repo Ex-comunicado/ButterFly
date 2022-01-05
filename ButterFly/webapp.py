ae6e3f03d50c3845c74263f391d82c3c57643059
Kathir Sudan <52427761+Ex-comunicado@users.noreply.github.com>
2022-01-05 15:51:21 +0530

    Add files via upload

--- ButterFly/webapp.py
+++ ButterFly/webapp.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template, flash, redirect, url_for
+from flask import Flask, render_template, flash, redirect, url_for, session, make_response, request
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_user, logout_user, login_required, current_user, LoginManager, UserMixin
 from flask_bcrypt import Bcrypt
+from flask_mysqldb import MySQL
+import os
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'password'
+app.config['MYSQL_DB'] = 'butterfly'
+
+mysql = MySQL(app)
 bcrypt = Bcrypt(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:password@localhost/butterfly"
 app.config['SECRET_KEY'] = 'bababooey'
@@ -63,11 +72,24 @@ class LoginForm(FlaskForm):
     username = StringField("Username: ")
     password = PasswordField("Password: ")
     submit = SubmitField("Login")
+
+class SQLiForm(FlaskForm):
+    id = StringField("Enter ID: ")
+    submit = SubmitField("Submit")
+
+class XSSform(FlaskForm):
+    name = StringField("Enter your name: ")
+    submit = SubmitField("Submit")
+
+class CominjForm(FlaskForm):
+    ipaddr = StringField("Enter a Domain Name or an IP Address: ")
+    submit = SubmitField("Submit")
 #------------------Forms------------------
 
 #------------------Routes------------------
 @app.route('/')
 def home_page():
+    cookie = request.cookies.get('role')
     return render_template('home.html')
 
 @app.route('/aboutus')
@@ -77,7 +99,16 @@ def about_us():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    resp = make_response(render_template('login.html', form=form))
     if form.validate_on_submit():
+        session['loggedin'] = True
+        session['user'] = form.username.data
+        if session['user'] == 'webadmin':
+            session['role'] = 'admin'
+            resp.set_cookie('role', 'admin')
+        else:
+            session ['role'] = 'user'
+            resp.set_cookie('role', 'user')
         attempted_user = Users.query.filter_by(username=form.username.data).first()
         if attempted_user and attempted_user.check_password(attempted_password=form.password.data):
             login_user(attempted_user)
@@ -85,7 +116,7 @@ def login():
             return redirect(url_for('home_page'))
         else:
             flash('Username or password is incorrect.', category="danger")
-    return render_template('login.html', form=form)
+    return resp
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -122,4 +153,59 @@ def page_not_found(e):
 @app.route('/vulns')
 def vuln_page():
     return render_template('vulnerabilities.html')
+
+@app.route('/sqli', methods=['GET','POST'])
+def sqli():
+    account=None
+    ID = None
+    form = SQLiForm()
+    if form.validate_on_submit():
+        ID = form.id.data
+        flash("Form submitted successfully!", category='success')
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT username FROM users WHERE id = '%s'" % (form.id.data))
+        account = cursor.fetchall()
+    return render_template('sqli.html', form=form, ID=ID, account=account)
+
+@app.route('/xss', methods=['GET','POST'])
+def xss():
+    name = None
+    form = XSSform()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted successfully!", category='success')
+    return render_template('xss.html', name = name, form = form)
+
+@app.route('/cominj', methods=['GET','POST'])
+def cominj():
+    ipaddr = None
+    result = None
+    form = CominjForm()
+    if form.validate_on_submit():
+        ipaddr = form.ipaddr.data
+        result = os.popen("nslookup "+ ipaddr).read()
+
+    return render_template('cominj.html', form=form, result=result, ipaddr=ipaddr)
+
+@app.route('/ac', methods=['GET','POST'])
+def ac():
+    if session['loggedin'] == True:
+        if request.cookies.get('role') == 'admin':
+            cursor = mysql.connection.cursor()
+            cursor.execute('SELECT id,username,email,password_hash FROM users')
+            deets = cursor.fetchall()
+        else:
+            return redirect('404.html')
+    return render_template('ac.html', deets=deets)
+
+@app.route('/htmlinj', methods=['GET', 'POST'])
+def htmlinj():
+    name = None
+    form = XSSform()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form submitted successfully!", category='success')
+    return render_template('htmlinj.html', name = name, form = form)
 #------------------Routes------------------
